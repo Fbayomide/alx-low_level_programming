d57895c52b5809dc090cffd0243e05b944745ab0
Fbayomide <fbayomide@gmail.com>
2023-09-13 15:40:20 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
-*main - 
-*
+*main - prints _putchar
 *Return:return 0
 */
-
-int main() {
-  printf("_putchar\n");
-  return 0;
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
 }
